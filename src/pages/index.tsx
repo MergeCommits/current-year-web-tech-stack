859dcaa9278347f1a3580680bbb72775abcccf51
Mark Jarjour <markjarjour@gmail.com>
2022-12-09 11:05:38 -0500

    Updated to t3 6.11.3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,6 +49,7 @@ const Home: NextPage = () => {
                                 "flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
                             }
                             href={"https://create.t3.gg/en/usage/first-steps"}
+                            target={"blank"}
                         >
                             <h3 className={"text-2xl font-bold"}>
                                 {"First Steps →"}
@@ -65,6 +66,7 @@ const Home: NextPage = () => {
                                 "flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
                             }
                             href={"https://create.t3.gg/en/introduction"}
+                            target={"blank"}
                         >
                             <h3 className={"text-2xl font-bold"}>
                                 {"Documentation →"}
