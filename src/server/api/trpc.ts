f6dfb80074869488cba0d1f2165042ab674b6963
Mark Jarjour <markjarjour@gmail.com>
2023-04-22 09:06:15 -0400

    Updated stack and dependencies.

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -35,9 +35,9 @@ type CreateContextOptions = {
  *
  * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
  */
-const createInnerTRPCContext = (opts: CreateContextOptions) => {
+const createInnerTRPCContext = ({ session }: CreateContextOptions) => {
     return {
-        session: opts.session,
+        session: session,
         prisma,
     };
 };
@@ -48,9 +48,10 @@ const createInnerTRPCContext = (opts: CreateContextOptions) => {
  *
  * @see https://trpc.io/docs/context
  */
-export const createTRPCContext = async (opts: CreateNextContextOptions) => {
-    const { req, res } = opts;
-
+export const createTRPCContext = async ({
+    req,
+    res,
+}: CreateNextContextOptions) => {
     // Get the session from the server using the getServerSession wrapper function
     const session = await getServerAuthSession({ req, res });
 
